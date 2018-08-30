e32daf6483628f15e083b6bc705489d559a435db
yeoping <yeop@cau.ac.kr>
2018-08-30 16:38:36 +0900

    #add struct stack_type

---
+++ stack_array/stackADT.h
@@ -0,0 +1,17 @@
+#ifndef STACKADT_H
+#define STACKADT_H
+
+typedef int Item;
+typedef stack_type* Stack;
+
+struct stack_type {
+	Item *contents;
+	int capacity;
+	int top;
+};
+
+//func
+
+
+///
+#endif // !STACKADT_H

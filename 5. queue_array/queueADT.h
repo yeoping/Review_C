baf4518ba2f3efd8ce7ffc67effb4b435e60a515
yeoping <yeop@cau.ac.kr>
2018-09-01 13:31:48 +0900

    # typedef

--- 5. queue_array/queueADT.h
+++ 5. queue_array/queueADT.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef QUEUEADT_H
+#define QUEUEADT_H
+#include <stdbool.h>
+
+typedef int Item;
+typedef struct queue_type* Queue;
+
+struct queue_type {
+	int *contents;
+	int front;
+	int rear;
+	int capacity;
+	int size;	//the number of datas
+};
+
+//func
+
+// // //
+#endif // !QUEUEADT_H
